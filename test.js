14d94edc8527242b4dab6f8e2a8520d2c358c000
AlexeiDenis <83495616+AlexeiDenis@users.noreply.github.com>
2021-09-09 19:07:58 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,38 @@
+/*console.log(Math.max(2,-6))*/
+
+/*let tn = Number(prompt("Pick a number"));
+console.log("Your number is the square root of " + tn*tn)*/
+
+
+//Aici daca inseram un text:"mere", acesta nu va mai fi luat in considerare la calculare
+
+/*let theNumber = Number(prompt("Pick a number"));
+
+if(!Number.isNaN(theNumber))
+{
+console.log("Your number is the square root of " + theNumber*theNumber)
+
+}
+else{
+    console.log("Nu ati inserat un numar")
+}*/
+
+
+/*if(!(1+1==2)){
+    console.log("Adv")
+}
+else{
+    console.log("Fals")
+}*/
+
+/*let nr = 0;
+while(nr <=12){
+    console.log(nr);
+    nr=nr+1;
+}*/
+
+/*let nr2 = 10
+for (let i=0; i<=nr2; i++){
+console.log(i);
+
+}*/
